31f48f2ae5493ac4a36865a8ea364d2cb26ea9e7
Тимур Скворцов <106396636+DOCtorascz@users.noreply.github.com>
2024-08-06 23:13:04 +0500

    112

--- mainServ.js
+++ mainServ.js
@@ -1,6 +1,6 @@
-import cors from 'cors';
-import axios from 'axios';
-import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const PORT = 3000;
@@ -8,23 +8,23 @@ const PORT = 3000;
 // Включение CORS
 app.use(cors());
 
-// Обработка запросов на /new_company
+// Обработка запросов на /proxy
 app.get('/proxy', async (req, res) => {
-// Формирование URL для запроса к API
-// Пример запроса: https://export-base.ru/api/company/?inn=0326008397&ogrn=1020300967642&key=xxxxxxxxxxxxxxx
-const apiUrl = `https://export-base.ru/api/company/?ogrn=1020300967642&key=66cb84e2bc25a2f`;
+    // Формирование URL для запроса к API
+    // Пример запроса: https://export-base.ru/api/company/?inn=0326008397&ogrn=1020300967642&key=xxxxxxxxxxxxxxx
+    const apiUrl = `https://export-base.ru/api/company/?ogrn=1020300967642&key=66cb84e2bc25a2f`;
 
-try {
-// Запрос к внешнему API
-const response = await axios.get(apiUrl);
-return res.json(response.data);
-} catch (error) {
-console.error('Ошибка при запросе к API:', error.message);
-return res.status(500).json({ error: 'Ошибка сервера' });
-}
+    try {
+        // Запрос к внешнему API
+        const response = await axios.get(apiUrl);
+        return res.json(response.data);
+    } catch (error) {
+        console.error('Ошибка при запросе к API:', error.message);
+        return res.status(500).json({ error: 'Ошибка сервера' });
+    }
 });
 
 // Запуск сервера
 app.listen(PORT, () => {
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
