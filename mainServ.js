3081303aec48ab8daab3667d804ef030009da223
Тимур Скворцов <106396636+DOCtorascz@users.noreply.github.com>
2024-08-06 23:41:55 +0500

    asd

--- mainServ.js
+++ mainServ.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios').create({
-    httpsAgent: new (require('https').Agent)({ rejectUnauthorized: false })
-});
+const https = require('https');
 
 const app = express();
 const PORT = 3000;
@@ -12,14 +10,20 @@ app.use(cors());
 
 // Обработка запросов на /proxy
 app.get('/proxy', async (req, res) => {
-    // Формирование URL для запроса к API
-    // Пример запроса: https://export-base.ru/api/company/?inn=0326008397&ogrn=1020300967642&key=xxxxxxxxxxxxxxx
     const apiUrl = `https://export-base.ru/api/company/?ogrn=1020300967642&key=66cb84e2bc25a2f`;
 
     try {
-        // Запрос к внешнему API
-        const response = await axios.get(apiUrl);
-        return res.json(response.data);
+        // Импортируем fetch динамически
+        const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+        const agent = new https.Agent({ rejectUnauthorized: false });
+        const response = await fetch(apiUrl, { agent });
+
+        if (!response.ok) {
+            throw new Error('Ошибка при запросе к API');
+        }
+
+        const data = await response.json();
+        return res.json(data);
     } catch (error) {
         console.error('Ошибка при запросе к API:', error.message);
         return res.status(500).json({ error: 'Ошибка сервера' });
